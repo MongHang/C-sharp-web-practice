87bbb5241a371bbef459c8974d6764c8d6954621
MongHang <kmh34529196@gmail.com>
2024-04-22 21:18:53 +0800

    Initial commit

---
+++ WebApplication2/Models/NightMarket.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class NightMarket
+    {
+        public string Id { get; set; } //Id 編號屬性
+        public string Name { get; set; } //Name名稱屬性
+        public string Address { get; set; } //Address地址數屬性
+    }
+}
